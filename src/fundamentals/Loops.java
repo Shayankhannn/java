3168408ed97f7d463a5a3f485a24b3489c284f84
shayankhannn <shayannkhannn007@gmail.com>
2026-05-15 07:51:39 -0700

    loops reversing string

---
+++ src/fundamentals/Loops.java
@@ -0,0 +1,22 @@
+package fundamentals;
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args)
+    {
+
+        // reversing string
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter the string you want to reverse");
+        String reverseString = keyboard.nextLine();
+
+            String reversed ="";
+        for (int i = reverseString.length()-1; i >=0 ; i--) {
+            char ch = reverseString.charAt(i);
+            reversed = reversed + ch ;
+        }
+        System.out.println(reversed);
+
+    }
+
+
+}
