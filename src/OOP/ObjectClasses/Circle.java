7632d850c0b03414e7da0af2afd6046e4b13dd27
shayankhannn <shayannkhannn007@gmail.com>
2026-05-17 03:17:52 -0700

    object classes modular

---
+++ src/OOP/ObjectClasses/Circle.java
@@ -0,0 +1,14 @@
+package OOP.ObjectClasses;
+
+public class Circle {
+    double radius;
+    public Circle (){
+        this.radius = 1.0;
+
+    }
+    public Circle(double radius){
+        this.radius=radius;
+
+    }
+}
+
---
+++ src/OOP/ObjectClasses/Rectangle.java
@@ -0,0 +1,13 @@
+package OOP.ObjectClasses;
+public class Rectangle {
+    double width;
+    double length;
+    public Rectangle (){
+        this.width = 1.0;
+        this.length = 1.0;
+    }
+    public Rectangle(double length,double width){
+        this.length=length;
+        this.width=width;
+    }
+}
