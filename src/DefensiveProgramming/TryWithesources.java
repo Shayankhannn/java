92087ddb649c871707771686777717bde63d7ad8
shayankhannn <shayannkhannn007@gmail.com>
2026-05-22 04:02:43 -0700

    Defensive programming -- try-with resource

---
+++ src/DefensiveProgramming/TryWithesources.java
@@ -0,0 +1,17 @@
+package DefensiveProgramming;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+public class TryWithesources {
+    public static void main(String[] args){
+        // try-with automatically close resources after code is executed
+        try (FileInputStream fis = new FileInputStream("providefilename.txt")){
+        int content;
+        while ((content = fis.read()) != -1){
+            System.out.println((char) content);
+        }
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
