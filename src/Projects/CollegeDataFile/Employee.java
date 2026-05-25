e98a86b380c4e5f2add7cf71adb6a9d0d653bbec
shayankhannn <shayannkhannn007@gmail.com>
2026-05-25 04:52:42 -0700

    file Project -- left incomplete

---
+++ src/Projects/CollegeDataFile/Employee.java
@@ -0,0 +1,22 @@
+package Projects.CollegeDataFile;
+
+public class Employee {
+    String name;
+    int age;
+    double salary;
+
+    public Employee(String name,int age,double salary){
+        this.name=name;
+        this.age=age;
+        this.salary=salary;
+    }
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + this.name + '\'' +
+                ", age=" + this.age +
+                ", salary= $" + this.salary +
+                '}';
+    }
+
+}
